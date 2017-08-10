a642e088339ecd40b0ab3ed70a2ea4727b88aa38
zhaobenx <ghostbenzhao@gmail.com>
2017-08-10 14:56:16 +0800

    Change nav list generating way, now the list can auto hide h2

--- js/application.js
+++ js/application.js
@@ -1,4 +1,3 @@
-
 var BlogDirectory = {
 
     /*
@@ -7,45 +6,45 @@ var BlogDirectory = {
      * reference to:
      * https://github.com/twbs/bootstrap/blob/master/docs/assets/js/src/application.js#L35
      */
-    setSideNavAffixing:function() {
+    setSideNavAffixing: function() {
 
         // Scrollspy
         var $window = $(window)
-        var $body   = $(document.body)
+        var $body = $(document.body)
 
         $body.scrollspy({
             target: '.bs-docs-sidebar'
         })
-        $window.on('load', function () {
+        $window.on('load', function() {
             $body.scrollspy('refresh')
         })
 
         // Kill links
-        $('.bs-docs-container [href=#]').click(function (e) {
+        $('.bs-docs-container [href=#]').click(function(e) {
             e.preventDefault()
         })
 
         // Sidenav affixing
-        setTimeout(function () {
+        setTimeout(function() {
             var $sideBar = $('.bs-docs-sidebar')
 
             $sideBar.affix({
                 offset: {
-                    top: function () {
-                        var offsetTop      = $sideBar.offset().top
-                        var sideBarMargin  = parseInt($sideBar.children(0).css('margin-top'), 100)
+                    top: function() {
+                        var offsetTop = $sideBar.offset().top
+                        var sideBarMargin = parseInt($sideBar.children(0).css('margin-top'), 100)
                         var navOuterHeight = $('.bs-docs-bar-top').height()
 
                         return (this.top = offsetTop - navOuterHeight - sideBarMargin)
                     },
-                    bottom: function () {
+                    bottom: function() {
                         return (this.bottom = 0 - $('.bs-docs-container').outerHeight(true))
                     }
                 }
             })
         }, 100)
 
-        setTimeout(function () {
+        setTimeout(function() {
             $('.bs-top').affix()
         }, 100)
     }, // end of setSideNavAffixing:function()
@@ -59,70 +58,79 @@ var BlogDirectory = {
      * processing: search header elements(h1,h2,h3) in `article_body_element`,
      * generate directory tree list, and put them into side_nav_element.
      */
-    createBlogDirectory:function (side_nav_element, article_body_element){
-        if(!article_body_element || article_body_element.length < 1 ||
+    createBlogDirectory: function(side_nav_element, article_body_element) {
+            if (!article_body_element || article_body_element.length < 1 ||
                 !side_nav_element) {
-            return false;
-        }
-
-        var nodes = article_body_element.find("h1,h2,h3");
-        var ulSideNav = side_nav_element;
-
-        $.each(nodes,function(){
-            var $this = $(this);
-
-            var nodetext = $this.text();
-            // There maybe HTML tags in header inner text, use regex to erase them
-            nodetext = nodetext.replace(/<\/?[^>]+>/g,"");
-            nodetext = nodetext.replace(/&nbsp;/ig, "");
-
-            // btw: Jekyll generates id for each header.
-            var nodeid = $this.attr("id");
-            if(!nodeid) {
-                nodeid = "top";
+                return false;
             }
 
-            var item_a = $("<a></a>");
-            item_a.attr("href", "#" + nodeid);
-            item_a.text(nodetext);
+            var nodes = article_body_element.find("h1,h2,h3");
+            var ulSideNav = side_nav_element;
 
-            var ret_li;
-            // wrapper: ul ( in the template, outside this code )
-            // h1: layer 1: li - a
-            // h2: layer 2: ul - li - a
-            // h3: layer 3: ul - ul - li - a
-            switch($this.get(0).tagName) {
-            case "H1":
-                var li_a = $("<li></li>").append(item_a);
-                ret_li = li_a;
-                break;
-            case "H2":
-                var li_a = $("<li></li>").append(item_a);
-                var nav_li_a = $("<ul class=\"nav\"></ul>").append(li_a);
-                ret_li = nav_li_a;
-                break;
-            case "H3":
-                var li_a = $("<li></li>").append(item_a);
-                var nav_li_a = $("<ul class=\"nav\"></ul>").append(li_a);
-                var nav_nav_li_a = $("<ul class=\"nav\"></ul>").append(nav_li_a);
-                ret_li = nav_nav_li_a;
-                break;
+            $.each(nodes, function() {
+                    var $this = $(this);
+
+                    var nodetext = $this.text();
+                    // There maybe HTML tags in header inner text, use regex to erase them
+                    nodetext = nodetext.replace(/<\/?[^>]+>/g, "");
+                    nodetext = nodetext.replace(/&nbsp;/ig, "");
+
+                    // btw: Jekyll generates id for each header.
+                    var nodeid = $this.attr("id");
+                    if (!nodeid) {
+                        nodeid = "top";
+                    }
+
+                    var item_a = "<a href=\"#" + nodeid + "\">" + nodetext + "</a>";
+                    /*$("<a></a>");
+                    item_a.attr("href", "#" + nodeid);
+                    item_a.text(nodetext);*/
+
+                    var ret_li;
+                    switch ($this.get(0).tagName) {
+                        case "H1":
+                            level = 1;
+                            if (lastlevel == 0) {
+                                ret_li = "<li>";
+                            } else if (lastlevel == 1) {
+                                ret_li = "</li><li>"
+                            } else if (lastlevel == 2) {
+                                ret_li = "</li></li></ul><li>"
+                            }
+                            break;
+                        case "H2":
+                            level = 2;
+                            if (lastlevel == 0) {
+                                ret_li = "<li><ul class=\"nav\"><li>";
+                            } else if (lastlevel == 1) {
+                                ret_li = "<ul class=\"nav\"><li>"
+                            } else if (lastlevel == 2) {
+                                ret_li = "</li><li>"
+                            }
+                            break;
+                    }
+                    ret_li += item_a;
+                    if (!ret_li) {
+                        // do nothing
+                    } else {
+                        html += ret_li;
+                    }
+                }) // end of each
+            if (lastlevel == 1) {
+                html += "</li>"
+            } else if (lastlevel == 2) {
+                html += "</li></li></ul>"
             }
+            //console.log(html);
+            ulSideNav.append(html); // end of each
 
-            if(!ret_li) {
-                // do nothing
-            } else {
-                ulSideNav.append(ret_li);
-            }
-        })  // end of each
-
-    } //end of createBlogDirectory:function()
+        } //end of createBlogDirectory:function()
 
 };
 
 
 jQuery(function($) {
-    $(document).ready( function() {
+    $(document).ready(function() {
         /* qrcode
         var opt = { text : window.location.href, width:150, height:150 };
         try {
