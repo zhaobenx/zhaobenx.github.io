673022849349da5fdac70310f068236677f65743
zhaobenx <ghostbenzhao@gmail.com>
2017-08-11 11:33:39 +0800

    Fix sidenav bar position error, by change the offset.

--- js/application.js
+++ js/application.js
@@ -32,7 +32,7 @@ var BlogDirectory = {
                 offset: {
                     top: function() {
                         var offsetTop = $sideBar.offset().top
-                        var sideBarMargin = parseInt($sideBar.children(0).css('margin-top'), 100)
+                        var sideBarMargin = parseInt($sideBar.children(0).css('margin-top'), 10)
                         var navOuterHeight = $('.bs-docs-bar-top').height()
 
                         return (this.top = offsetTop - navOuterHeight - sideBarMargin)
