a0cbe55a44318c4fc509130827776d07fd9a8660
zhaobenx <ghostbenzhao@gmail.com>
2017-10-09 21:31:18 +0800

    add file name function to newpost.py

--- _drafts/newpost.py
+++ _drafts/newpost.py
@@ -14,9 +14,10 @@ def main():
     title = ''
     while not title:
         title = input("请输入文章标题:\n")
+    file_name = input("请输入文件名称，空默认为标题:\n") or title
     tag = input("请输入标签 可为空:\n").split()
     date = time.strftime("%Y-%m-%d-", time.localtime())
-    with open(date+title.replace(' ','-').replace('　','-') + ".md","w",encoding='utf8') as file:
+    with open(date+file_name.replace(' ','-').replace('　','-') + ".md","w",encoding='utf8') as file:
         file.write(template.format(title=title,tag=tag))
 
     print("Done! Enjoy your writing!")
